fad18184aa6aefa1c06d997bb6c09ae741403250
Dylan Eck <akdylaneck@gmail.com>
2022-06-06 16:07:01 -0600

    parameter entry boxes are now initially populated with default values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from venv import create
 import pandas as pd
 from PIL import (
     Image,
@@ -42,8 +43,15 @@ def load_file():
     file_entry_box.insert(index=0, string=filename)
 
 def generate_stickers():
-    global parameters
-    get_parameters()
+    update_parameter_variables()
+    global parameter_entry_dict
+
+    sticker_width = parameter_entry_dict['sticker width']
+    sticker_height = parameter_entry_dict['sticker height']
+    dot_radius = parameter_entry_dict['dot radius']
+    dot_spacing = parameter_entry_dict['dot spacing']
+    font_size = parameter_entry_dict['font size']
+    highlight_padding = parameter_entry_dict['highlight padding']
 
     global filename
     if not filename:
@@ -62,32 +70,32 @@ def generate_stickers():
         color = row[1]
         subgroup = row[2]
 
-        image = Image.new(mode="RGB", size=(parameters['sticker_width'], parameters['sticker_height']), color=color)
+        image = Image.new(mode="RGB", size=(sticker_width, sticker_height), color=color)
         draw = ImageDraw.Draw(image)
 
         if subgroup == 'polka dot':
-            num_dots_horiz = int(parameters['sticker_width'] / parameters['dot_spacing'])
-            num_dots_vert = int(parameters['sticker_height'] / parameters['dot_spacing'])
+            num_dots_horiz = int(sticker_width / dot_spacing)
+            num_dots_vert = int(sticker_height / dot_spacing)
 
-            dwidth = (num_dots_horiz - 1) * parameters['dot_spacing']
-            dheight = (num_dots_vert - 1) * parameters['dot_spacing']
+            dwidth = (num_dots_horiz - 1) * dot_spacing
+            dheight = (num_dots_vert - 1) * dot_spacing
 
             for i in range(num_dots_horiz):
                 for j in range(num_dots_vert):
-                    x = (i * parameters['dot_spacing']) + ((parameters['sticker_width'] - dwidth) / 2)
-                    y = (j * parameters['dot_spacing']) + ((parameters['sticker_height'] - dheight) / 2)
+                    x = (i * dot_spacing) + ((sticker_width - dwidth) / 2)
+                    y = (j * dot_spacing) + ((sticker_height - dheight) / 2)
 
-                    p1 = (x - parameters['dot_radius'], y - parameters['dot_radius'])
-                    p2 = (x + parameters['dot_radius'], y + parameters['dot_radius'])
+                    p1 = (x - dot_radius, y - dot_radius)
+                    p2 = (x + dot_radius, y + dot_radius)
 
                     draw.ellipse((p1, p2), fill='white')
 
-        draw.rectangle([(0, 0), (parameters['sticker_width'], parameters['sticker_height'])], outline='white', width=4, fill=None)
+        draw.rectangle([(0, 0), (sticker_width, sticker_height)], outline='white', width=4, fill=None)
 
-        font = ImageFont.truetype("Arial.ttf", parameters['font_size'])
-        x0, y0, x1, y1 = draw.textbbox((parameters['sticker_width'] / 2, parameters['sticker_height'] / 2), plate_number, font=font, anchor='mm')
-        draw.rectangle([(x0 - parameters['highlight_padding'], y0 - parameters['highlight_padding']), (x1 + parameters['highlight_padding'], y1 + parameters['highlight_padding'])], outline=(110, 110, 100), width=4, fill='white')
-        draw.text((parameters['sticker_width'] / 2, parameters['sticker_height'] / 2), plate_number, fill="Black", font=font, anchor='mm')
+        font = ImageFont.truetype("Arial.ttf", font_size)
+        x0, y0, x1, y1 = draw.textbbox((sticker_width / 2, sticker_height / 2), plate_number, font=font, anchor='mm')
+        draw.rectangle([(x0 - highlight_padding, y0 - highlight_padding), (x1 + highlight_padding, y1 + highlight_padding)], outline=(110, 110, 100), width=4, fill='white')
+        draw.text((sticker_width / 2, sticker_height / 2), plate_number, fill="Black", font=font, anchor='mm')
 
         if len(image_buffer) == 24:
             sticker_sheet = create_sheet(image_buffer)
@@ -97,12 +105,12 @@ def generate_stickers():
         else:
             image_buffer.append(image)
 
-def get_parameters():
-    global parameters
-    for key in parameters:
-        for widget in parameters[key].winfo_children():
+def update_parameter_variables():
+    global parameter_entry_dict
+    for key in parameter_entry_dict:
+        for widget in parameter_entry_dict[key].entry_frame.winfo_children():
             if widget.winfo_class() == 'Entry':
-                parameters[key] = int(widget.get())
+                parameter_entry_dict[key] = int(widget.get())
 
 def create_labeled_entry_box(parent, text, font):
     frame = Frame(parent)
@@ -112,15 +120,20 @@ def create_labeled_entry_box(parent, text, font):
     entry.grid(row=0, column=1)
     return frame
 
+class Parameter:
+    def __init__(self, name, default_value, entry_frame):
+        self.name = name
+        self.default_value = default_value
+        self.entry_frame = entry_frame
+
 if __name__ == '__main__':
-    global parameters
-    parameters = {
-        'font_size': None,           # 50
-        'highlight_padding': None,   # 10
-        'sticker_width': None,       # 300
-        'sticker_height': None,      # 256
-        'dot_radius': None,          # 15
-        'dot_spacing': None          # 50
+    parameter_info = {
+        'font size': 50,
+        'highlight padding': 10,
+        'sticker width': 300,
+        'sticker height': 256,
+        'dot radius': 15,
+        'dot spacing': 50
     }
 
     global filename
@@ -131,14 +144,19 @@ if __name__ == '__main__':
     ### window items ###
     root = Tk()
     root.title(string='number plate sticker tool')
-
     wwidth = 320
     wheight = 240 
     root.geometry(str(wwidth) + 'x' + str(wheight))
     root.resizable(False, False)
 
-    for key in parameters:
-        parameters[key] = create_labeled_entry_box(root, key + ':', font)
+    global parameter_entry_dict
+    parameter_entry_dict = {}
+    for key in parameter_info:
+        name = key
+        default_value = parameter_info[key]
+        entry_frame = create_labeled_entry_box(root, name + ':', font)
+        parameter = Parameter(name, default_value, entry_frame)
+        parameter_entry_dict[key] = parameter
 
     global file_entry_box
     file_entry_box = Entry(root, width=41, font=font)
@@ -148,8 +166,12 @@ if __name__ == '__main__':
 
     ### layout ###
     row = 0
-    for entry in parameters.values():
-        entry.grid(row=row, column=0, columnspan=2)
+    for parameter in parameter_entry_dict.values():
+        for widget in parameter.entry_frame.winfo_children():
+            if widget.winfo_class() == 'Entry':
+                widget.insert(0, str(parameter.default_value))
+
+        parameter.entry_frame.grid(row=row, column=0, columnspan=2)
         row += 1
 
     file_entry_box.grid(row=row, column=0, columnspan=2)
